bb5e6061bc458ac82b48062acf3b811b822870a1
jstalinko <alinkodotid@gmail.com>
2026-06-07 19:57:55 +0700

    feat: add test send document whatsapp

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Inertia\Inertia;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\KaryawanController;
 use App\Http\Controllers\SlipController;
+use App\Http\Services\PiwapiService;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -27,6 +28,31 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('slips/download-paid-pdf', [SlipController::class, 'downloadPaidPdf'])->name('slips.downloadPaidPdf');
     Route::get('slips/{slip}/download-pdf', [SlipController::class, 'downloadIndividualPdf'])->name('slips.downloadIndividualPdf');
 });
+
+
+Route::get('/whatsapp/test', function () {
+    $service = app(PiwapiService::class);
+     $slipsPath = storage_path('app/public/slips');
+    $pdfFiles = glob($slipsPath . '/*.pdf');
+    
+    if (empty($pdfFiles)) {
+        $this->error("No PDF files found in {$slipsPath}!");
+        return;
+    }
+    
+    $selectedPdf = $pdfFiles[0];
+    $pdfFilename = basename($selectedPdf);
+    $pdfUrl = url('/storage/slips/' . $pdfFilename);
+    $res = $service
+        ->setRecipient('087857580910')
+        ->data([
+            'name' => 'John Doe',
+        ])->message('Halo {{name}}')
+        ->document($pdfUrl, 'pdf')
+        ->send();
+    return $res;
+});
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
